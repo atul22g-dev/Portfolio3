faa5c95ae00b5c2a737aa7442c634b5f0d32afaf
Atul22g <atul22g857086@gmail.com>
2022-11-08 17:40:35 +0530

    Add smoot scrooling

--- js/script.js
+++ js/script.js
@@ -1,8 +1,48 @@
+// Navbar
 let nav_btn = document.getElementsByClassName("nav-btn")[0];
-let btn = document.getElementById("btn")
-let list = document.getElementById("list")
+let btn = document.getElementById("btn");
+let list = document.getElementById("list");
 
 nav_btn.addEventListener("click", () => {
-  btn.classList.toggle("is-active")
-  list.classList.toggle("list2")
+  btn.classList.toggle("is-active");
+  list.classList.toggle("list2");
+});
+
+// ========================================
+//  smooth scrolling
+// ========================================
+
+// Home
+
+const homeSection = document.querySelector(".section-home");
+
+document.querySelector(".home-link").addEventListener("click", (e) => {
+  e.preventDefault();
+  homeSection.scrollIntoView({ behavior: "smooth" });
+});
+
+// About
+
+const aboutSection = document.querySelector(".section-about");
+
+document.querySelector(".about-link").addEventListener("click", (e) => {
+  e.preventDefault();
+  aboutSection.scrollIntoView({ behavior: "smooth" });
+});
+
+// project
+
+const projectSection = document.querySelector(".section-project");
+
+document.querySelector(".project-link").addEventListener("click", (e) => {
+  e.preventDefault();
+  projectSection.scrollIntoView({ behavior: "smooth" });
+});
+// contact
+
+const contctSection = document.querySelector(".section-contact");
+
+document.querySelector(".contact-link").addEventListener("click", (e) => {
+  e.preventDefault();
+  contctSection.scrollIntoView({ behavior: "smooth" });
 });

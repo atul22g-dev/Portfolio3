142864c40e68dfa2b5287bb63104c50f7cf31546
Atugatran <atugatran@gmail.com>
2022-11-30 02:52:31 +0530

    Add toggle mode

--- js/script.js
+++ js/script.js
@@ -46,3 +46,52 @@ document.querySelector(".contact-link").addEventListener("click", (e) => {
   e.preventDefault();
   contctSection.scrollIntoView({ behavior: "smooth" });
 });
+
+// Toggles
+const themeSwitcher = document.getElementById("icon");
+const body = document.getElementsByTagName("body")[0];
+
+const themeSwitcher2 = document.getElementById("modebtn2");
+themeSwitcher2.addEventListener("click", () => {
+  // console.log("dark");
+  if (body.classList == "dark-mode") {
+    body.classList.add("dark-mode");
+    body.classList.remove("dark-mode");
+    localStorage.setItem("theme", "light-mode");
+  } else {
+    body.classList.remove("dark-mode");
+    body.classList.add("dark-mode");
+    localStorage.setItem("theme", "dark-mode");
+  }
+});
+
+// With Local-storage
+
+themeSwitcher.checked = false;
+
+function clickHandler() {
+  if (this.checked) {
+    body.classList.remove("dark-mode");
+    body.classList.add("dark-mode");
+    localStorage.setItem("theme", "dark-mode");
+  } else {
+    body.classList.add("dark-mode");
+    body.classList.remove("dark-mode");
+    localStorage.setItem("theme", "light-mode");
+  }
+}
+themeSwitcher.addEventListener("click", clickHandler);
+
+function checkTheme() {
+  const localStorageTheme = localStorage.getItem("theme");
+
+  if (localStorageTheme !== null && localStorageTheme === "dark-mode") {
+    // set the theme of body
+    body.className = localStorageTheme;
+    // adjust the slider position
+    const themeSwitch = document.getElementById("icon");
+    themeSwitch.checked = true;
+  }
+}
+
+window.onload = checkTheme();
